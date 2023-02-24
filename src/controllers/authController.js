17c4e07134d7368e6e0b4e661af64517711d128c
sttallev99 <sttallev99@gmail.com>
2023-02-24 08:39:49 +0200

    create User model

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -4,7 +4,16 @@ const getRegister = (req, res) => {
     res.render('auth/register');
 }
 
+const getLogin = (req, res) => {
+    res.render('auth/login');
+}
+
+const postregister = (req, res) => {
+
+}
+
 router.get('/register', getRegister);
+router.get('/login', getLogin)
 
 
 module.exports = router;
